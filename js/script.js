57cf7fdbe3d79b3ea3844d0cdd91717061901949
Filippo Garota <filogarota101@gmail.com>
2021-03-26 16:19:44 +0100

    Bottone che aggiunge nuovo elemento

--- js/script.js
+++ js/script.js
@@ -2,11 +2,17 @@ var app = new Vue(
  {
    el: '#app',
    data: {
-     tasks:['Fare la spesa', "Tagliare l'erba", "Pulire il garage"]
+     tasks:['Fare la spesa', "Tagliare l'erba", "Pulire il garage"],
+     newTask: ""
    },
    methods: {
      deleteItem: function(tasksIndex) {
        this.tasks.splice(tasksIndex, 1)
+     },
+     addTask: function() {
+       if (this.newTask != "") {
+         this.tasks.push(this.newTask)
+       }
      }
    }
  }
