317f06620b314810ee1713817b2ea315653c2d2f
Filippo Garota <filogarota101@gmail.com>
2021-03-26 15:49:39 +0100

    todolist

---
+++ js/script.js
@@ -0,0 +1,13 @@
+var app = new Vue(
+ {
+   el: '#app',
+   data: {
+     tasks:['Fare la spesa', "Tagliare l'erba", "Pulire il garage"]
+   },
+   methods: {
+     deleteItem: function(tasksIndex) {
+      
+     }
+   }
+ }
+);
